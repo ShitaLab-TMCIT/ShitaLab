d7d6752a62b4dad5a6a61b740969abcc67a7d630
ryoww <ryo.shimozawa.0121@gmail.com>
2024-11-07 20:23:46 +0900

    TK GOD

--- path_to_json.py
+++ path_to_json.py
@@ -1,51 +1,59 @@
-import os
-import json
+from collections import defaultdict
 from datetime import datetime
+import json
+import os
+from pathlib import Path
 
 def get_file_timestamp(path):
     timestamp = os.path.getmtime(path)
+    # return datetime.now(ZoneInfo('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M:%S') 
     return datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
 
-def remove_extension(file_name):
-    return os.path.splitext(file_name)[0]
+def update(d:dict, u:dict):
+    """再帰dictをupdateするヤツ"""
+    for k, v in u.items():
+        if isinstance(v, dict):
+            d[k] = update(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
 
-def dir_to_dict(path):
-    dir_dict = {}
-    dirs = []
-    files = []
-    
-    with os.scandir(path) as it:
-        for entry in it:
-            if entry.is_dir():
-                # ディレクトリ名が 'assets' の場合はスキップ
-                if entry.name != 'assets':
-                    dirs.append(entry)
-            else:
-                # ファイルの拡張子が .md の場合のみリストに追加
-                if entry.name.endswith('.md'):
-                    files.append(entry) 
+def set_recursive_dir(dirs:list[str], value:dict):
+    """再帰dictにvalue入れるヤツ"""
+    structure=defaultdict(dict)
+    _structure = structure
+    for dir in dirs:
+        _structure[dir] = _structure.get(dir, defaultdict(dict))
+        _structure = _structure[dir]
+    _structure.update(value)
+    return structure
 
-    for directory in sorted(dirs, key=lambda e: os.path.getmtime(e.path), reverse=True):
-        dir_dict[directory.name] = dir_to_dict(directory.path)
+def top_dict(d:dict):
+    """dictを上にもってくヤツ"""
+    result={}
+    queue = {}
+    for k, v in d.items():
+        if isinstance(v, dict):
+            result[k] = top_dict(v)
+        else:
+            queue[k] = v
+    result.update(queue)
+    return result
 
-    for file in sorted(files, key=lambda e: os.path.getmtime(e.path), reverse=True):
-        file_name_without_extension = remove_extension(file.name)
-        dir_dict[file_name_without_extension] = get_file_timestamp(file.path)
 
-    return dir_dict
+directory_to_scan = "./public/md/"
+output_json_file = "./public/file_path.json"
 
-def save_structure_to_json(structure, output_file):
-    with open(output_file, 'w', encoding='utf-8') as f:
-        json.dump(structure, f, ensure_ascii=False, indent=4)
+root = Path(directory_to_scan)
+directory_structure = {}
 
-if __name__ == "__main__":
-    directory_to_scan = "./public/md/"
-    output_json_file = "./public/file_path.json"
-    
-    directory_structure = dir_to_dict(directory_to_scan)
-    
-    save_structure_to_json(directory_structure, output_json_file)
-    
-    print(directory_structure)
-    
-    print(f"saved '{output_json_file}'")
+md_files = sorted(root.glob('**/*.md'), key=lambda e: os.path.getmtime(e), reverse=True)
+
+for file in md_files:
+    parts = file.relative_to(root).parts[:-1]
+    structure_dict=set_recursive_dir(parts, {file.stem:get_file_timestamp(file)})
+    directory_structure=update(directory_structure, structure_dict)
+directory_structure = top_dict(directory_structure)
+
+with open('public/file_path.json', 'w') as f:
+    json.dump(directory_structure, f, ensure_ascii=False, indent=4)
